8504d4db2f4784e9748debd37df1e171e11c0238
Garesu <44981193+gperilli@users.noreply.github.com>
2020-02-03 13:13:13 +0900

    Update p007.js

--- p007/p007.js
+++ p007/p007.js
@@ -50,9 +50,9 @@ AnnotType: "Annot",
 	BtnChar3: "digit3",
 
 	//Examples
-	EX1: {ENG: "I'm going to go to the airport.", ITA: "Andrò all'aeroporto.", JPN: "私は空港に行くつもりです。",},
-	EX2: {ENG: "The airport is over there.", ITA: "L'aeroporto è laggiù.", JPN: "空港はそこにあります。",},
-	EX3: {ENG: "How far is it to the airport?", ITA: "Quanto dista l'aeroporto?", JPN: "空港まではどれぐらい遠いですか？",},
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
     //Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -112,9 +112,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "block", ENG: "buildings", ITA: "edifici", JPN: "建物",},
 	
-	EX1: {ENG: "Send a message to the control tower.", ITA: "Invii un messaggio alla torre di controllo.", JPN: "管制塔にメッセージを送信します。",},
-	EX2: {ENG: "The pilot asks permission to depart from the control tower.", ITA: "Il pilota chiede alla torre di controllo il permesso per partire.", JPN: "パイロットは管制塔に出発する許可を求めます。",},
-	EX3: {ENG: "The pilots asked the control tower to go back to the airport.", ITA: "I piloti hanno chiesto alla torre di controllo di poter tornare all’aeroporto.", JPN: "パイロットは、管制塔に空港に戻るように頼んだ。",},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -125,17 +126,18 @@ AnnotType: "Annot",
     itaLhyph: "torre-di-controllo", itaLplus: "torre+di+controllo", itaForv: "torre_di_controllo/#it",
     jpnLpercen: "%E7%AE%A1%E5%88%B6%E5%A1%94", jpnLgoo: "48697/meaning/m1u/", jpnForv: "管制塔/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+	// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -171,9 +173,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "block", ENG: "verbs", ITA: "verbi", JPN: "動詞",},
 	
-	EX1: {ENG: "Looks like we have to make an emergency landing.", ITA: "Dovremo tentare un atterraggio di emergenza.", JPN: "緊急着陸する必要があるようです。",},
-	EX2: {ENG: "The plane made a perfect landing.", ITA: "L'aereo effettuò un atterraggio perfetto.", JPN: "飛行機は完璧な着陸をした。",},
-	EX3: {ENG: "They say most airplane accidents occur at landing or takeoff stages.", ITA: "Si dice che la maggior parte degli incidenti aerei avviene nelle fasi di atterraggio o di decollo.", JPN: "ほとんどの飛行機事故は着陸または離陸の時に発生すると言います。",},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -184,17 +187,18 @@ AnnotType: "Annot",
     itaLhyph: "atterraggio", itaLplus: "atterraggio", itaForv: "atterraggio/#it",
     jpnLpercen: "%E7%9D%80%E9%99%B8", jpnLgoo: "142609/meaning/m1u/", jpnForv: "着陸/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -228,9 +232,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "block", ENG: "verbs", ITA: "verbi", JPN: "動詞",},
 	
-	EX1: {ENG: "Gold squadron, begin take-off procedure.", ITA: "Squadra Oro, iniziare procedura di decollo.", JPN: "ゴールド飛行隊、離陸手続きを開始する。",},
-	EX2: {ENG: "The crew prepares for take off.", ITA: "L'equipaggio si prepara al decollo.", JPN: "乗組員は離陸を準備する。",},
-	EX3: {ENG: "We may not serve beverages during take-off or landing.", ITA: "Non possiamo servire da bere durante il decollo o l'atterraggio.", JPN: "離陸または着陸中に飲み物を提供することはできません。",},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -241,17 +246,18 @@ AnnotType: "Annot",
     itaLhyph: "decollo", itaLplus: "decollo", itaForv: "decollo/#it",
     jpnLpercen: "%E9%9B%A2%E9%99%B8", jpnLgoo: "233036/meaning/m1u/", jpnForv: "離陸/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+    // Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -286,9 +292,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	
-	EX1: {ENG: "I looked at the departure board.", ITA: "Ho guardato il tabellone delle partenze.", JPN: "私は出発案内表示板を見た。",},
-	EX2: {ENG: "The departure board is full of cancelled flights.", ITA: "Il tabellone delle partenze e degli arrivi è denso di voli cancellati.", JPN: "出発案内表示板にはキャンセルされたフライトがいっぱいです。",},
-	EX3: {ENG: "There are 27 flights in arrival that have been cancelled, while 24 are those disappeared from the departure board.", ITA: "Sono al momento 27 i voli in arrivo che sono stati cancellati mentre 24 sono quelli scomparsi dal tabellone delle partenze.", JPN: "27便の到着がはキャンセルされ、24便は出発案内表示板から消えています。",},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -299,17 +306,18 @@ AnnotType: "Annot",
     itaLhyph: "tabellone-delle-partenze", itaLplus: "tabellone+delle+partenze", itaForv: "tabellone/#it",
     jpnLpercen: "%E6%8E%B2%E7%A4%BA%E6%9D%BF", jpnLgoo: "66523/meaning/m1u/", jpnForv: "表示板/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+	// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -344,9 +352,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "furniture", ITA: "mobili", JPN: "家具",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	
-	EX1: {ENG: 'Where is my <span class="wihlight">seat</span>?', ITA: 'Dov&#39;è il mio <span class="wihlight">sedile</span>?', JPN: '私の<span class="wihlight">席</span>はどこですか？',},
-	EX2: {ENG: 'I was in the front <span class="wihlight">seat</span>.', ITA: 'Ero sul <span class="wihlight">sedile</span> anteriore.', JPN: '私は前方座<span class="wihlight">席</span>にいた。',},
-	EX3: {ENG: 'It was under the <span class="wihlight">seat<span> in my vehicle.', ITA: 'Era sotto il <span class="wihlight">sedile</span> della mia auto.', JPN: 'それは私の車の座<span class="wihlight">席</span>の下にあった。',},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/251304", ITA: "https://tatoeba.org/ita/sentences/show/836253", JPN: "https://tatoeba.org/jpn/sentences/show/163208",},
@@ -357,17 +366,18 @@ AnnotType: "Annot",
     itaLhyph: "sedile", itaLplus: "sedile", itaForv: "sedile/#it",
     jpnLpercen: "%E5%B8%AD", jpnLgoo: "123323/meaning/m1u/", jpnForv: "席/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+	// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
    //Audio
@@ -402,9 +412,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	
-	EX1: {ENG: 'I will buy the <span class="wihlight">tickets</span>.', ITA: 'Comprerò i <span class="wihlight">biglietti</span>.', JPN: '私が<span class="wihlight">チケット</span>を買うよ。',},
-	EX2: {ENG: 'Do you have the <span class="wihlight">tickets</span>?', ITA: 'Avete i <span class="wihlight">biglietti</span>?', JPN: '<span class="wihlight">チケット</span>を持っていますか？',},
-	EX3: {ENG: 'He was checking a <span class="wihlight">ticket</span>.', ITA: 'Controllava un <span class="wihlight">biglietto</span>.', JPN: '彼は<span class="wihlight">チケット</span>を確認していた。',},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -415,17 +426,18 @@ AnnotType: "Annot",
     itaLhyph: "biglietto", itaLplus: "biglietto", itaForv: "biglietto/#it",
     jpnLpercen: "%E3%83%81%E3%82%B1%E3%83%83%E3%83%88", jpnLgoo: "141577/meaning/m1u/", jpnForv: "チケット/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+	// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -460,9 +472,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	
-	EX1: {ENG: 'There were fifty <span class="wihlight">passengers</span> on the plane.', ITA: 'C&#39;erano cinquanta <span class="wihlight">passeggeri</span> sull&#39;aereo.', JPN: '飛行機には50人の<span class="wihlight">乗客がいた。',},
-	EX2: {ENG: 'The train was full of <span class="wihlight">passengers</span>.', ITA: 'Il treno era pieno di <span class="wihlight">passeggeri</span>.', JPN: '列車には<span class="wihlight">乗客</span>がいっぱいだった。',},
-	EX3: {ENG: 'He was a <span class="wihlight">passenger</span> on the Flight 627.', ITA: 'Era un <span class="wihlight">passeggero</span> del volo 627.', JPN: '彼は627便の<span class="wihlight">乗客</span>だった。',},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -473,17 +486,18 @@ AnnotType: "Annot",
     itaLhyph: "passeggero", itaLplus: "passeggero", itaForv: "passeggero/#it",
     jpnLpercen: "%E4%B9%97%E5%AE%A2", jpnLgoo: "107916/meaning/m1u/", jpnForv: "乗客/#ja",
 	
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
@@ -517,9 +531,10 @@ AnnotType: "Annot",
 	semtag2: {Dsp: "block", ENG: "airport", ITA: "aeroporto", JPN: "空港",},
 	semtag3: {Dsp: "", ENG: "", ITA: "", JPN: "",},
 	
-	EX1: {ENG: 'Take this <span class="wihlight">suitcase</span> and put it on the bed.', ITA: 'Prendi questa <span class="wihlight">valigia</span> e mettila sul letto.', JPN: 'この<span class="wihlight">スーツケース</span>を持ってベッドの上に置いてください。',},
-	EX2: {ENG: 'Open your <span class="wihlight">suitcase</span>.', ITA: 'Apri la tua <span class="wihlight">valigia</span>.', JPN: '<span class="wihlight">スーツケース</span>を開けてください。',},
-	EX3: {ENG: 'Which is your <span class="wihlight">suitcase</span>?', ITA: 'Qual è la tua <span class="wihlight">valigia</span>?', JPN: 'どれがあなたの<span class="wihlight">スーツケース</span>ですか？',},
+	//Examples
+	EX1: {ENG: '', ITA: '', JPN: '',},
+	EX2: {ENG: '', ITA: '', JPN: '',},
+	EX3: {ENG: '', ITA: '', JPN: '',},
 
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -530,17 +545,18 @@ AnnotType: "Annot",
     itaLhyph: "valigia", itaLplus: "valigia", itaForv: "valigia/#it",
     jpnLpercen: "%E3%82%B9%E3%83%BC%E3%83%84%E3%82%B1%E3%83%BC%E3%82%B9", jpnLgoo: "117095/meaning/m1u/", jpnForv: "スーツケース/#ja",
 
-	PoSp1: {ENG: {ENG: "adjective", ITA: "adjective", JPN: "adjective",}, 
-			ITA: {ENG: "aggetivo", ITA: "aggetivo", JPN: "aggetivo",},
-			JPN: {ENG: "形容詞", ITA: "形容詞", JPN: "形容詞",},
+	// Part of Speech Data
+	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
+			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
+			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
 			},
-	PoSp2: {ENG: {ENG: "", ITA: "(invar)", JPN: "i/na",}, 
-			ITA: {ENG: "", ITA: "()", JPN: "i/na",},
-			JPN: {ENG: "", ITA: "()", JPN: "i/na",},
+	PoSp2: {ENG: {ENG: "", ITA: "masc.", JPN: "",}, 
+			ITA: {ENG: "", ITA: "masc.", JPN: "",},
+			JPN: {ENG: "名詞", ITA: "名詞", JPN: "名詞",},
 			},
 	PoSp3: {ENG: {ENG: "", ITA: "", JPN: "",}, 
 			ITA: {ENG: "", ITA: "", JPN: "",},
-			JPN: {ENG: "", ITA: "", JPN: "",},
+			JPN: {ENG: "", ITA: "男性形", JPN: "",},
 			},
 
     //Audio
