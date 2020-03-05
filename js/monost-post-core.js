8261c4cf931280ba0cb3693e8fdd670d82d37a14
Garesu <44981193+gperilli@users.noreply.github.com>
2020-03-05 13:19:34 +0900

    Update monost-post-core.js

--- js/monost-post-core.js
+++ js/monost-post-core.js
@@ -1,9 +1,29 @@
+function setposter(pIdX) {
+	
+	for(var i = 0; i < 9; ++i) {
+	pIdX[i]["imgfile"] = "p".concat(pIdX["No"], "/monop", pIdX["No"], "-wi", i, ".svg");
+	pIdX[i]["AudENGWI"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ENG.wav");
+	pIdX[i]["AudENGEx1"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ENGex1.wav");
+	pIdX[i]["AudENGEx2"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ENGex2.wav");
+	pIdX[i]["AudENGEx3"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ENGex3.wav");
+	pIdX[i]["AudITAWI"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ITA.wav");
+	pIdX[i]["AudITAEx1"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ITAex1.wav");
+	pIdX[i]["AudITAEx2"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ITAex2.wav");
+	pIdX[i]["AudITAEx3"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "ITAex3.wav");
+	pIdX[i]["AudJPNWI"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "JPN.wav");
+	pIdX[i]["AudJPNEx1"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "JPNex1.wav");
+	pIdX[i]["AudJPNEx2"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "JPNex2.wav");
+	pIdX[i]["AudJPNEx3"] = "p".concat(pIdX["No"], "/p", pIdX["No"], "wi", i, "JPNex3.wav");
+	}
+}
+
 function pageload() {
 
   //p00X = window[pnovar];
-  p00X.SetPost();//Load in repetetive object values
-
-  pId = p00X; //(not necessary?)
+  //p00X.SetPost();//Load in repetetive object values
+  
+  pId = p00X;
+  setposter(pId)
   pno = pId.No;
   //alert(pno);
   //Prepare Main User Interface Template
