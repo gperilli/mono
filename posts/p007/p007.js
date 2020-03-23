0296f57bbccb27f66ba518a50f195f9538e04a9c
Garesu <44981193+gperilli@users.noreply.github.com>
2020-03-24 00:54:06 +0900

    Update p007.js

--- posts/p007/p007.js
+++ posts/p007/p007.js
@@ -14,7 +14,10 @@ AnnotType: "Annot",
 	//Word Item
 	WI: {ENG: "internal organs", ITA: "organi interni", JPN: "内臓",},
 	WIr: {ENG: "/ɪnˈtɜːnl ˈɔːgənz/", ITA: "/ˈɔrɡani inˈtɛrni/", JPN: "naizou", JPNRu: "<ruby>内臓<rt>ないぞう</rt></ruby>",},
-	
+	WIcmp: {ENG: "1", EngCmp1: "internal", EngCmp2: "organs",
+			ITA: "2", ItaCmp1: "organi", ItaCmp2: "interni",
+			JPN: "2", JpnCmp1: "",},
+			
 	//Sematic Tags
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -31,10 +34,10 @@ AnnotType: "Annot",
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
     // Dictionary search suffixes
-    engLhyph: "internal-organs", engLperc: "internal%20organs", engLplus: "internal+organs", engForv: "internal_organs/#en",
-    itaLhyph: "organi-interni", itaLplus: "organi+interni", itaForv: "organi_interni/#it",
-    jpnLpercen: "%E5%86%85%E8%87%93", jpnLgoo: "55838/meaning/m1u/", jpnForv: "内臓/#ja",
-
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
+	
     // Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
 		ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
@@ -68,6 +71,9 @@ AnnotType: "Annot",
 
 	WI: {ENG: "intestine", ITA: "intestino", JPN: "腸",},
 	WIr: {ENG: "/ɪnˈtɛstɪn/", ITA: "/intesˈtino/", JPN: "chou", JPNRu: "<ruby>腸<rt>ちょう</rt></ruby>",},
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
 	
     semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -83,9 +89,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "",},
 
-    engLhyph: "intestine", engLperc: "intestine", engLplus: "intestine", engForv: "intestine/#en",
-    itaLhyph: "intestino", itaLplus: "intestino", itaForv: "intestino/#it",
-    jpnLpercen: "%E8%85%B8", jpnLgoo: "48649/meaning/m1u/", jpnForv: "腸/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
 	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -120,7 +127,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "heart", ITA: "cuore", JPN: "心臓",},
 	WIr: {ENG: "/hɑːt/", ITA: "/ˈkwɔre/", JPN: "shinzou", JPNRu: "<ruby心臓<rt>しんぞう</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+			
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -135,11 +145,12 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/251243", ITA: "https://tatoeba.org/eng/sentences/show/602491", JPN: "https://tatoeba.org/eng/sentences/show/163269",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "heart", engLperc: "heart", engLplus: "heart", engForv: "heart/#en",
-    itaLhyph: "cuore", itaLplus: "cuore", itaForv: "cuore/#it",
-    jpnLpercen: "%E5%BF%83%E8%87%93", jpnLgoo: "37826/meaning/m1u/", jpnForv: "心臓/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
-// Part of Speech Data
+	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
 			ITA: {ENG: "sostantivo", ITA: "sostantivo", JPN: "sostantivo",},
 			JPN: {ENG: "可算", ITA: "可算", JPN: "可算",},
@@ -172,7 +183,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "kidney", ITA: "rene", JPN: "腎臓",},
 	WIr: {ENG: "/ˈkɪdni/", ITA: "/ˈrɛne/", JPN: "jinzou", JPNRu: "<ruby>腎臓<rt>じんぞう</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+	
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -187,9 +201,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "kidney", engLperc: "kidney", engLplus: "kidney", engForv: "kidney/#en",
-    itaLhyph: "rene", itaLplus: "rene", itaForv: "rene/#it",
-    jpnLpercen: "%E8%85%8E%E8%87%93", jpnLgoo: "37854/meaning/m1u/", jpnForv: "腎臓/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
     // Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -224,7 +239,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "liver", ITA: "fegato", JPN: "肝臓",},
 	WIr: {ENG: "/ˈlɪvə/", ITA: "/ˈfeɡato/", JPN: "kanzou", JPNRu: "<ruby>肝臓<rt>かんぞう</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+	
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -239,9 +257,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "liver", engLperc: "liver", engLplus: "liver", engForv: "liver/#en",
-    itaLhyph: "fegato", itaLplus: "fegato", itaForv: "fegato/#it",
-    jpnLpercen: "%E8%82%9D%E8%87%93", jpnLgoo: "14828/meaning/m1u/", jpnForv: "肝臓/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
 	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -269,7 +288,6 @@ AnnotType: "Annot",
 	
 	imgbckg: "wht",	
 
-
 	LnContPosLeft: "5%", LnContPosTop: "62%",
 	BtnPosLeft: "4.5%", BtnPosTop: "12%",
 	LnPosX1: "50%", LnPosY1: "75%",
@@ -277,6 +295,9 @@ AnnotType: "Annot",
 
 	WI: {ENG: "lung", ITA: "polmone", JPN: "肺",},
 	WIr: {ENG: "/lʌŋ/", ITA: "/polˈmone/", JPN: "hai", JPNRu: "<ruby>肺<rt>はい</rt></ruby>",},
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
 
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -292,9 +313,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "lung", engLperc: "lung", engLplus: "lung", engForv: "lung/#en",
-    itaLhyph: "polmone", itaLplus: "polmone", itaForv: "polmone/#it",
-    jpnLpercen: "%E8%82%BA", jpnLgoo: "59475/meaning/m1u/", jpnForv: "肺/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
 	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -329,7 +351,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "stomach", ITA: "stomaco", JPN: "胃",},
 	WIr: {ENG: "/ˈstʌmək/", ITA: "/ˈstɔmako/", JPN: "i", JPNRu: "<ruby>胃<rt>い</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+	
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -344,9 +369,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "stomach", engLperc: "stomach", engLplus: "stomach", engForv: "stomach/#en",
-    itaLhyph: "stomaco", itaLplus: "stomaco", itaForv: "stomaco/#it",
-    jpnLpercen: "%E8%83%83", jpnLgoo: "2447/meaning/m1u/", jpnForv: "胃/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
 	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -381,7 +407,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "neuron", ITA: "neurone", JPN: "神経細胞",},
 	WIr: {ENG: "/ˈnjʊərɒn/", ITA: "/neuˈrone/", JPN: "shinkeisaibou", JPNRu: "<ruby>神経<rt>しんけい</rt>細胞<rt>さいぼう</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+			
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -396,9 +425,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "neuron", engLperc: "neuron", engLplus: "neuron", engForv: "neuron/#en",
-    itaLhyph: "neurone", itaLplus: "neurone", itaForv: "neurone/#it",
-    jpnLpercen: "%E7%A5%9E%E7%B5%8C%E7%B4%B0%E8%83%9E", jpnLgoo: "37463/meaning/m1u/", jpnForv: "神経細胞/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 	
 // Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
@@ -433,7 +463,10 @@ AnnotType: "Annot",
 
 	WI: {ENG: "brain", ITA: "cervello", JPN: "脳",},
 	WIr: {ENG: "/breɪn/", ITA: "/tʃerˈvɛllo/", JPN: "nou", JPNRu: "<ruby>脳<rt>のう</rt></ruby>",},
-
+	WIcmp: {ENG: "1", EngCmp1: "",
+			ITA: "1", ItaCmp1: "",
+			JPN: "1", JpnCmp1: "",},
+	
 	semtag1: {Dsp: "block", ENG: "body", ITA: "corpo", JPN: "体",},
 	semtag2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	semtag3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -448,9 +481,10 @@ AnnotType: "Annot",
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx3: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 
-    engLhyph: "brain", engLperc: "brain", engLplus: "brain", engForv: "brain/#en",
-    itaLhyph: "cervello", itaLplus: "cervello", itaForv: "cervello/#it",
-    jpnLpercen: "%E8%84%B3", jpnLgoo: "58764/meaning/m1u/", jpnForv: "脳/#ja",
+    // Dictionary search suffixes
+    engLhyph: "", engLperc: "", engLplus: "", engForv: "",
+    itaLhyph: "", itaLplus: "", itaForv: "",
+    jpnLpercen: "", jpnLgoo: "", jpnForv: "",
 
 	// Part of Speech Data
 	PoSp1: {ENG: {ENG: "noun", ITA: "noun", JPN: "noun",}, 
