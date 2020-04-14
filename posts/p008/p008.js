162918a4ebe3c0794a6b99a251a61f3c7347fd76
Garesu <44981193+gperilli@users.noreply.github.com>
2020-04-14 23:41:25 +0900

    Update p008.js

--- posts/p008/p008.js
+++ posts/p008/p008.js
@@ -31,6 +31,10 @@ AnnotType: "Annot",
 	EX2: {ENG: 'I live on this <span class="wihlight">planet</span>.', ITA: 'Vivo su questo <span class="wihlight">pianeta</span>.', JPN: '私はこの<span class="wihlight">惑星</span>に住んでいる。',},
 	EX3: {ENG: 'The Earth, Mars and Jupiter are <span class="wihlight">planets</span>.', ITA: 'La Terra, Marte e Giove sono <span class="wihlight">pianeti</span>.', JPN: '地球や火星、木星は<span class="wihlight">惑星</span>である。',},
 	
+	EXAud1: {ENG: "Planets move around a fixed star.", ITA: "I pianeti ruotano attorno ad una stella fissa.", JPN: "惑星は恒星の周りを回る。",},
+	EXAud2: {ENG: "I live on this planet.", ITA: "Vivo su questo pianeta.", JPN: "私はこの惑星に住んでいる。",},
+	EXAud3: {ENG: "The Earth, Mars and Jupiter are planets.", ITA: "La Terra, Marte e Giove sono pianeti.", JPN: "地球や火星、木星は惑星である。",},
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "display-block", ENG: "https://tatoeba.org/eng/sentences/show/326588", ITA: "https://tatoeba.org/eng/sentences/show/1616478", JPN: "https://tatoeba.org/eng/sentences/show/77131",},
 	TatSrcEx2: {Dsp: "display-block", ENG: "https://tatoeba.org/eng/sentences/show/2693531", ITA: "https://tatoeba.org/eng/sentences/show/3008221", JPN: "https://tatoeba.org/eng/sentences/show/2684280",},	
@@ -86,7 +90,11 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The planet closet to the sun is <span class="wihlight">Mercury</span>.', ITA: 'Il pianeta più vicino al sole è <span class="wihlight">Mercurio</span>.', JPN: '太陽に一番近い惑星は水星である。',},
 	EX2: {ENG: '<span class="wihlight">Mercury</span> is a rocky planet.', ITA: '<span class="wihlight">Mercurio</span> è un pianeta roccioso.', JPN: '<span class="wihlight">水星</span>は岩だらけの惑星です。',},
 	EX3: {ENG: '<span class="wihlight">Mercury</span> does not have moons.', ITA: '<span class="wihlight">Mercurio</span> non possiede satelliti.', JPN: '<span class="wihlight">水星</span>には月がありません。',},
-
+	
+	EXAud1: {ENG: "The planet closet to the sun is Mercury.", ITA: "Il pianeta più vicino al sole è Mercurio.", JPN: "太陽に一番近い惑星は水星である。",},
+	EXAud2: {ENG: "Mercury is a rocky planet.", ITA: "Mercurio è un pianeta roccioso.", JPN: "水星は岩だらけの惑星です。",},
+	EXAud3: {ENG: "Mercury does not have moons.", ITA: "Mercurio non possiede satelliti.", JPN: "水星には月がありません。",},
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -142,6 +150,10 @@ AnnotType: "Annot",
 	EX1: {ENG: '<span class="wihlight">Venus</span> is the second planet of the solar system.', ITA: '<span class="wihlight">Venere</span> è il secondo pianeta del Sistema solare.', JPN: '<span class="wihlight">金星</span>は、太陽系で2番目の惑星。',},
 	EX2: {ENG: '<span class="wihlight">Venus</span> has much more carbon dioxide than the Earth.', ITA: '<span class="wihlight">Venere</span> è molto più ricca di anidride carbonica rispetto a la terra.', JPN: '<span class="wihlight">金星</span>は地球よりもはるかに多くの二酸化炭素を持っています。',},
 	EX3: {ENG: '<span class="wihlight">Venus</span> is the most luminous planet in the sky.', ITA: '<span class="wihlight">Venere</span> è il pianeta più luminoso nel cielo.', JPN: '<span class="wihlight">金星</span>は空で最も明るい惑星です。',},
+	
+	EXAud1: {ENG: "Venus is the second planet of the solar system.", ITA: "Venere è il secondo pianeta del Sistema solare.", JPN: "金星は、太陽系で2番目の惑星。",},
+	EXAud2: {ENG: "Venus has much more carbon dioxide than the Earth.", ITA: "Venere è molto più ricca di anidride carbonica rispetto a la terra.", JPN: "金星は地球よりもはるかに多くの二酸化炭素を持っています。",},
+	EXAud3: {ENG: "Venus is the most luminous planet in the sky.", ITA: "Venere è il pianeta più luminoso nel cielo.", JPN: "金星は空で最も明るい惑星です。",},
 	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -199,6 +211,10 @@ AnnotType: "Annot",
 	EX2: {ENG: 'The <span class="wihlight">Earth</span> is round.', ITA: 'La <span class="wihlight">Terra</span> è rotonda.', JPN: '<span class="wihlight">地球</span>は丸い。',},
 	EX3: {ENG: 'The <span class="wihlight">Earth</span> formed 4.5 billion years ago.', ITA: 'la <span class="wihlight">Terra</span> si è formata 4,5 miliardi di anni fa.', JPN: '<span class="wihlight">地球</span>は45億年前に形成されました。',},
 	
+	EXAud1: {ENG: "The Earth is the second planet of the solar system.", ITA: "La Terra è il terzo pianeta del sistema solare.", JPN: "地球は、太陽系で3番目の惑星。",},
+	EXAud2: {ENG: "The Earth is round.", ITA: "La Terra è rotonda.", JPN: "地球は丸い。",},
+	EXAud3: {ENG: "The Earth formed 4.5 billion years ago.", ITA: "la Terra si è formata 4,5 miliardi di anni fa.", JPN: "地球は45億年前に形成されました。",},
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "display-block", ENG: "https://tatoeba.org/eng/sentences/show/277121", ITA: "https://tatoeba.org/eng/sentences/show/2157930", JPN: "https://tatoeba.org/eng/sentences/show/126969",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -254,6 +270,11 @@ AnnotType: "Annot",
 	EX1: {ENG: '<span class="wihlight">Mars</span> is the fourth planet of the solar system.', ITA: '<span class="wihlight">Marte</span> è il quarto pianeta del Sistema solare.', JPN: '<span class="wihlight">火星</span>は、太陽系で４番目の惑星。',},
 	EX2: {ENG: '<span class="wihlight">Mars</span> has two moons.', ITA: '<span class="wihlight">Marte</span> ha due lune.', JPN: '<span class="wihlight">火星</span>は二つ衛星がある。',},
 	EX3: {ENG: 'Is there any life on <span class="wihlight">Mars</span>?', ITA: 'C&#39;è vita su <span class="wihlight">Marte</span>?', JPN: '<span class="wihlight">火星</span>に生命はありますか？',},
+	
+	EXAud1: {ENG: "Mars is the fourth planet of the solar system.", ITA: "Marte è il quarto pianeta del Sistema solare.", JPN: "火星は、太陽系で４番目の惑星。",},
+	EXAud2: {ENG: "Mars has two moons.", ITA: "Marte ha due lune.", JPN: "火星は二つ衛星がある。",},
+	EXAud3: {ENG: "Is there any life on Mars?", ITA: "C'è vita su Marte?", JPN: "火星に生命はありますか？",},
+	
 	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -311,6 +332,11 @@ AnnotType: "Annot",
 	EX2: {ENG: '<span class="wihlight">Jupiter</span> is the biggest planet of the solar system.', ITA: '<span class="wihlight">Giove</span> è il pianeta più grande del Sistema solare.', JPN: '<span class="wihlight">木星</span>は太陽系の最大の惑星です。',},
 	EX3: {ENG: '<span class="wihlight">Jupiter</span>, like Saturn, Uranus, and Neptune is a gas planet.', ITA: '<span class="wihlight">Giove</span>, come Saturno, Urano e Nettuno, è un pianeta gassoso.', JPN: '<span class="wihlight">木星</span>は、土星、天王星、海王星のようなガス惑星です。',},
 	
+	EXAud1: {ENG: "Jupiter is the fifth planet of the solar system.", ITA: "Giove è il quinto pianeta del Sistema solare.", JPN: "火星は、太陽系で５番目の惑星。",},
+	EXAud2: {ENG: "Jupiter is the biggest planet of the solar system.", ITA: "Giove è il pianeta più grande del Sistema solare.", JPN: "木星は太陽系の最大の惑星です。",},
+	EXAud3: {ENG: "Jupiter, like Saturn, Uranus, and Neptune is a gas planet.", ITA: "Giove, come Saturno, Urano e Nettuno, è un pianeta gassoso.", JPN: "木星は、土星、天王星、海王星のようなガス惑星です。",},
+	
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -366,6 +392,10 @@ AnnotType: "Annot",
 	EX1: {ENG: '<span class="wihlight">Saturn</span> is the sixth planet of the solar system.', ITA: '<span class="wihlight">Saturno</span> è il sesto pianeta del sistema solare.', JPN: '<span class="wihlight">土星</span>は、太陽系で６番目の惑星。',},
 	EX2: {ENG: '<span class="wihlight">Saturn</span> is the second largest planet after Jupiter in the solar system.', ITA: '<span class="wihlight">Saturno</span> è il secondo pianeta per grandezza dopo Giove nel sistema solare.', JPN: '<span class="wihlight">土星</span>は、太陽系で木星に次いで2番目に大きい惑星です。',},
 	EX3: {ENG: 'The rings of <span class="wihlight">Saturn</span> are made from billions of small objects.', ITA: 'Gli anelli di <span class="wihlight">Saturno</span> sono composti da miliardi di piccoli oggetti.', JPN: '<span class="wihlight">土星</span>の輪は、数十億の小さな物体からできています。',},
+	
+	EXAud1: {ENG: "Saturn is the sixth planet of the solar system.", ITA: "Saturno è il sesto pianeta del sistema solare.", JPN: "土星は、太陽系で６番目の惑星。",},
+	EXAud2: {ENG: "Saturn is the second largest planet after Jupiter in the solar system.", ITA: "Saturno è il secondo pianeta per grandezza dopo Giove nel sistema solare.", JPN: "土星は、太陽系で木星に次いで2番目に大きい惑星です。",},
+	EXAud3: {ENG: "The rings of Saturn are made from billions of small objects.", ITA: "Gli anelli di Saturno sono composti da miliardi di piccoli oggetti.", JPN: "土星の輪は、数十億の小さな物体からできています。",},
 	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -423,6 +453,11 @@ AnnotType: "Annot",
 	EX2: {ENG: '<span class="wihlight">Uranus</span> was discovered by acccident in 1781 by William Herschel.', ITA: '<span class="wihlight">Urano</span> venne scoperto casualmente al telescopio da William Herschel, nel 1781', JPN: '<span class="wihlight">天王星</span>は、ウィリアム・ハーシェルによって1781年に事故によって発見されました。',},
 	EX3: {ENG: 'There are many small moons that rotate around <span class="wihlight">Uranus</span>.', ITA: 'Ci sono tante piccole lune che ruotano intorno a <span class="wihlight">Urano</span>.', JPN: '<span class="wihlight">天王星</span>の周りを回転する多くの小さな月があります。',},
 	
+	EXAud1: {ENG: "Uranus is the seventh planet of the solar system.", ITA: "Urano è il settimo pianeta del sistema solare.", JPN: "天皇制は、太陽系で７番目の惑星。",},
+	EXAud2: {ENG: "Uranus was discovered by acccident in 1781 by William Herschel.", ITA: "Urano venne scoperto casualmente al telescopio da William Herschel, nel 1781", JPN: "天王星は、ウィリアム・ハーシェルによって1781年に事故によって発見されました。",},
+	EXAud3: {ENG: "There are many small moons that rotate around Uranus.", ITA: "Ci sono tante piccole lune che ruotano intorno a Urano.", JPN: "天王星の周りを回転する多くの小さな月があります。",},
+	
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -478,6 +513,10 @@ AnnotType: "Annot",
 	EX1: {ENG: '<span class="wihlight">Neptune</span> is the eight planet of the solar system.', ITA: '<span class="wihlight">Nettuno</span> è il ottavo pianeta del sistema solare.', JPN: '<span class="wihlight">海王星</span>は、太陽系で８番目の惑星。',},
 	EX2: {ENG: '<span class="wihlight">Neptune</span> is the fourth largest planet of the solar system.', ITA: '<span class="wihlight">Nettuno</span> è il quarto pianeta più grande del sistema solare.', JPN: '<span class="wihlight">海王星</span>は、太陽系で4番目に大きな惑星です。',},
 	EX3: {ENG: 'The first observation of <span class="wihlight">Neptune</span> was made in 1612 by Galileo Galilei.', ITA: 'La prima osservazione di <span class="wihlight">Nettuno</span> fu effettuata da Galileo Galilei nel 1612.', JPN: '<span class="wihlight">海王星</span>の最初の観測は、ガリレオガリレイによって1612年に行われました。',},
+	
+	EXAud1: {ENG: "Neptune is the eight planet of the solar system.", ITA: "Nettuno è il ottavo pianeta del sistema solare.", JPN: "海王星は、太陽系で８番目の惑星。",},
+	EXAud2: {ENG: "Neptune is the fourth largest planet of the solar system.", ITA: "Nettuno è il quarto pianeta più grande del sistema solare.", JPN: "海王星は、太陽系で4番目に大きな惑星です。",},
+	EXAud3: {ENG: "The first observation of Neptune was made in 1612 by Galileo Galilei.", ITA: "La prima osservazione di Nettuno fu effettuata da Galileo Galilei nel 1612.", JPN: "海王星の最初の観測は、ガリレオガリレイによって1612年に行われました。",},
 	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
