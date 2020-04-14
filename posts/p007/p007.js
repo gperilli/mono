9bc1dacccd816794bef1ac9447662344a7b00e7b
Garesu <44981193+gperilli@users.noreply.github.com>
2020-04-14 15:20:01 +0900

    Update p007.js

--- posts/p007/p007.js
+++ posts/p007/p007.js
@@ -29,7 +29,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The stomach is one of the internal <span class="wihlight">organs</span>.', ITA: 'Lo stomaco è uno degli <span class="wihlight">organi</span> interni.', JPN: '胃は<span class="wihlight">内臓</span>の一つである。',},
 	EX2: {ENG: 'The <span class="wihlight">internal organs</span> are the organs of the human body  not visible from outside.', ITA: 'Gli <span class="wihlight">organi interni</span> sono gli organi del corpo umano non visibili dall&#39;esterno', JPN: '<span class="wihlight">内臓</span>とは、外部からは見えない人体の臓器です。',},
 	EX3: {ENG: 'The liver is the biggest <span class="wihlight">internal organ</span> of the human body.', ITA: 'Il fegato è l&#39;<span class="wihlight">organo</span> interno più grande del corpo umano', JPN: '肝臓は人体の最大の<span class="wihlight">内臓</span>です。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "The stomach is one of the internal organs.", ITA: "Lo stomaco è uno degli organi interni.", JPN: "胃は内臓の一つである。",},
+	EXAud2: {ENG: "The internal organs are the organs of the human body  not visible from outside.", ITA: "Gli organi interni sono gli organi del corpo umano non visibili dall'esterno", JPN: "内臓とは、外部からは見えない人体の臓器です。",},
+	EXAud3: {ENG: "The liver is the biggest internal organ of the human body.", ITA: "Il fegato è l'organo interno più grande del corpo umano", JPN: "肝臓は人体の最大の内臓です。",},
 
+	
     //Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/28152", ITA: "https://tatoeba.org/eng/sentences/show/5707988", JPN: "https://tatoeba.org/eng/sentences/show/190992",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -85,7 +91,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The small <span class="wihlight">intestine</span> is about seven meters long.', ITA: 'Il piccolo <span class="wihlight">intestino</span> è lungo circa sette metri.', JPN: '小<span class="wihlight">腸</span>は７メートルくらいだ。',},
 	EX2: {ENG: 'The <span class="wihlight">intestine</span> is the final part of the digestive system.', ITA: 'L&#39;<span class="wihlight">intestino</span> è la parte finale dell&#39;apparato digerente.', JPN: '<span class="wihlight">腸</span>は消化器系の最後の部分です。',},
 	EX3: {ENG: 'The <span class="wihlight">intestine</span> is an organ always in movement.', ITA: 'L&#39;<span class="wihlight">intestino</span> è un organo sempre in movimento.', JPN: '<span class="wihlight">腸</span>は常に動いている器官です。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "The small intestine is about seven meters long.", ITA: "Il piccolo intestino è lungo circa sette metri.", JPN: "小腸は７メートルくらいだ。",},
+	EXAud2: {ENG: "The intestine is the final part of the digestive system.", ITA: "L'intestino è la parte finale dell'apparato digerente.", JPN: "腸は消化器系の最後の部分です。",},
+	EXAud3: {ENG: "The intestine is an organ always in movement.", ITA: "L'intestino è un organo sempre in movimento.", JPN: "腸は常に動いている器官です。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/2275312", ITA: "https://tatoeba.org/eng/sentences/show/3942977", JPN: "https://tatoeba.org/eng/sentences/show/2279526",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -141,7 +153,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'He put his hand on his <span class="wihlight">heart</span>.', ITA: 'Mise la mano sul suo <span class="wihlight">cuore</span>.', JPN: '彼は<span class="wihlight">心臓</span>に手を当てた。',},
 	EX2: {ENG: 'My <span class="wihlight">heart</span> stopped beating.', ITA: 'Il mio <span class="wihlight">cuore</span> ha smesso di battere.', JPN: '私の<span class="wihlight">心臓</span>が止まるような気がした。',},
 	EX3: {ENG: 'The <span class="wihlight">heart</span> is like a pump.', ITA: 'Il <span class="wihlight">cuore</span> è come un pompa.', JPN: '<span class="wihlight">心臓</span>はポンプのようなものです。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "He put his hand on his heart.", ITA: "Mise la mano sul suo cuore.", JPN: "彼は心臓に手を当てた。",},
+	EXAud2: {ENG: "My heart stopped beating.", ITA: "Il mio cuore ha smesso di battere.", JPN: "私の心臓が止まるような気がした。",},
+	EXAud3: {ENG: "The heart is like a pump.", ITA: "Il cuore è come un pompa.", JPN: "心臓はポンプのようなものです。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/313264", ITA: "https://tatoeba.org/eng/sentences/show/602483", JPN: "https://tatoeba.org/eng/sentences/show/375957",},
 	TatSrcEx2: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/251243", ITA: "https://tatoeba.org/eng/sentences/show/602491", JPN: "https://tatoeba.org/eng/sentences/show/163269",},
@@ -197,7 +215,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'I have <span class="wihlight">kidney</span> trouble.', ITA: 'Ho dei problemi ai <span class="wihlight">reni</span>.', JPN: '<span class="wihlight">腎臓</span>が悪いのです。',},
 	EX2: {ENG: 'The <span class="wihlight">kidney</span> has the form of an American bean.', ITA: 'Il <span class="wihlight">rene</span> ha la forma di un fagiolo americano.', JPN: '<span class="wihlight">腎臓</span>はアメリカ豆の形をしています。',},
 	EX3: {ENG: 'The primary function of the <span class="wihlight">kidneys</span> is to filter the blood.', ITA: 'La funzione principale dei <span class="wihlight">reni</span> è quella di filtrare il sangue.', JPN: '<span class="wihlight">腎臓</span>の主な機能は、血液をろ過することです。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "I have kidney trouble.", ITA: "Ho dei problemi ai reni.", JPN: "腎臓が悪いのです。",},
+	EXAud2: {ENG: "The kidney has the form of an American bean.", ITA: "Il rene ha la forma di un fagiolo americano.", JPN: "腎臓はアメリカ豆の形をしています。",},
+	EXAud3: {ENG: "The primary function of the kidneys is to filter the blood.", ITA: "La funzione principale dei reni è quella di filtrare il sangue.", JPN: "腎臓の主な機能は、血液をろ過することです。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/270666", ITA: "https://tatoeba.org/eng/sentences/show/5387384", JPN: "https://tatoeba.org/eng/sentences/show/143900",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -253,7 +277,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The <span class="wihlight">liver</span> is the most complex organ of the human body.', ITA: 'Il <span class="wihlight">fegato</span> è l&#39;organo più complesso del corpo umano.', JPN: '<span class="wihlight">肝臓</span>は人体の最も複雑な器官です。',},
 	EX2: {ENG: 'The <span class="wihlight">liver</span> weighs about 1000-1500 grams.', ITA: 'Il <span class="wihlight">fegato</span> pesa circa 1000-1500 grammi.', JPN: '<span class="wihlight">肝臓</span>の重量は約1000〜1500グラムです。',},
 	EX3: {ENG: 'Some symptoms can indicate a problem with the <span class="wihlight">liver</span>.', ITA: 'Alcuni sintomi possono indicare un problema al <span class="wihlight">fegato</span>.', JPN: '一部の症状は、<span class="wihlight">肝臓</span>の問題を示している可能性があります。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "The liver is the most complEXAud organ of the human body.", ITA: "Il fegato è l'organo più complesso del corpo umano.", JPN: "肝臓は人体の最も複雑な器官です。",},
+	EXAud2: {ENG: "The liver weighs about 1000-1500 grams.", ITA: "Il fegato pesa circa 1000-1500 grammi.", JPN: "肝臓の重量は約1000〜1500グラムです。",},
+	EXAud3: {ENG: "Some symptoms can indicate a problem with the liver.", ITA: "Alcuni sintomi possono indicare un problema al fegato.", JPN: "一部の症状は、肝臓の問題を示している可能性があります。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -309,7 +339,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'He died of <span class="wihlight">lung</span> cancer.', ITA: 'Morì di cancro ai <span class="wihlight">polmoni</span>.', JPN: '彼は<span class="wihlight">肺</span>がんで死んだ。',},
 	EX2: {ENG: 'The human body contains two <span class="wihlight">lungs</span>.', ITA: ' Il corpo umano contiene due <span class="wihlight">polmoni</span>.', JPN: '人体には2つの<span class="wihlight">肺</span>があります。',},
 	EX3: {ENG: 'The <span class="wihlight">lungs</span> have the task of providing oxygen to the body.', ITA: 'I <span class="wihlight">polmoni</span> hanno il compito di fornire ossigeno al corpo.', JPN: '<span class="wihlight">肺</span>には、身体に酸素を供給する役割があります。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "He died of lung cancer.", ITA: "Morì di cancro ai polmoni.", JPN: "彼は肺がんで死んだ。",},
+	EXAud2: {ENG: "The human body contains two lungs.", ITA: " Il corpo umano contiene due polmoni.", JPN: "人体には2つの肺があります。",},
+	EXAud3: {ENG: "The lungs have the task of providing oxygen to the body.", ITA: "I polmoni hanno il compito di fornire ossigeno al corpo.", JPN: "肺には、身体に酸素を供給する役割があります。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "inline-block", ENG: "https://tatoeba.org/eng/sentences/show/302426", ITA: "https://tatoeba.org/eng/sentences/show/1299366", JPN: "https://tatoeba.org/eng/sentences/show/101271",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -365,7 +401,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'Do you have a <span class="wihlight">stomach</span>ache?', ITA: 'Hai il mal di <span class="wihlight">stomaco</span>?', JPN: 'お<span class="wihlight">腹</span>痛いの？',},
 	EX2: {ENG: 'My <span class="wihlight">stomach</span> is full.', ITA: 'Ho lo <span class="wihlight">stomaco</span> pieno.', JPN: 'お<span class="wihlight">腹</span>がいっぱいです。',},
 	EX3: {ENG: 'When you eat, the food goes quickly down into the <span class="wihlight">stomach</span>.', ITA: 'Quando mangi, il cibo scende rapidamente nello <span class="wihlight">stomaco</span>.', JPN: '食べると、食べ物はすぐに<span class="wihlight">胃</span>に落ちます。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "Do you have a stomachache?", ITA: "Hai il mal di stomaco?", JPN: "お腹痛いの？",},
+	EXAud2: {ENG: "My stomach is full.", ITA: "Ho lo stomaco pieno.", JPN: "お腹がいっぱいです。",},
+	EXAud3: {ENG: "When you eat, the food goes quickly down into the stomach.", ITA: "Quando mangi, il cibo scende rapidamente nello stomaco.", JPN: "食べると、食べ物はすぐに胃に落ちます。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "https://tatoeba.org/eng/sentences/show/2786926", ITA: "https://tatoeba.org/eng/sentences/show/2715737", JPN: "https://tatoeba.org/eng/sentences/show/2771616",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -421,7 +463,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The <span class="wihlight">neuron</span> is a cell of the nervous system.', ITA: 'Il <span class="wihlight">neurone</span> è una cellula del sistema nervoso.', JPN: '<span class="wihlight">神経細胞</span>は神経系の細胞です。',},
 	EX2: {ENG: 'There are over 86 billion <span class="wihlight">neurons</span> in the brain.', ITA: 'Ci sono oltre 86 miliardi di <span class="wihlight">neuroni</span> nel cervello', JPN: '脳には860億以上の<span class="wihlight">神経細胞</span>があります。',},
 	EX3: {ENG: '<span class="wihlight">Neurons</span> put into motion other organs.', ITA: 'I <span class="wihlight">neuroni</span> mettono in moto altri organi,', JPN: '<span class="wihlight">神経細胞</span>は他の器官を動かします。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "The neuron is a cell of the nervous system.", ITA: "Il neurone è una cellula del sistema nervoso.", JPN: "神経細胞は神経系の細胞です。",},
+	EXAud2: {ENG: "There are over 86 billion neurons in the brain.", ITA: "Ci sono oltre 86 miliardi di neuroni nel cervello", JPN: "脳には860億以上の神経細胞があります。",},
+	EXAud3: {ENG: "Neurons put into motion other organs.", ITA: "I neuroni mettono in moto altri organi,", JPN: "神経細胞は他の器官を動かします。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
@@ -477,7 +525,13 @@ AnnotType: "Annot",
 	EX1: {ENG: 'The <span class="wihlight">brain</span> is found inside the skull.', ITA: 'Il <span class="wihlight">cervello</span> si trova all&#39;interno del cranio.', JPN: '<span class="wihlight">脳</span>は頭蓋骨の中にあります。',},
 	EX2: {ENG: 'The The <span class="wihlight">brain</span> is a complex organ that is part of the central nervous system.', ITA: 'Il <span class="wihlight">cervello</span> é un organo complesso che è parte del sistema nervoso centrale.', JPN: '<span class="wihlight">脳</span>は中枢神経系の一部である複雑な器官です。',},
 	EX3: {ENG: 'He died after an operation to the The <span class="wihlight">brain</span>.', ITA: 'É morto dopo un intervento al The <span class="wihlight">cervello</span>', JPN: '彼は<span class="wihlight">脳</span>への手術後に死亡した。',},
+	
+	//EXAudamples
+	EXAud1: {ENG: "The brain is found inside the skull.", ITA: "Il cervello si trova all'interno del cranio.", JPN: "脳は頭蓋骨の中にあります。",},
+	EXAud2: {ENG: "The The brain is a complEXAud organ that is part of the central nervous system.", ITA: "Il cervello é un organo complesso che è parte del sistema nervoso centrale.", JPN: "脳は中枢神経系の一部である複雑な器官です。",},
+	EXAud3: {ENG: "He died after an operation to the The brain.", ITA: "É morto dopo un intervento al The cervello", JPN: "彼は脳への手術後に死亡した。",},
 
+	
 	//Tatoeba Srcs
 	TatSrcEx1: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
 	TatSrcEx2: {Dsp: "none", ENG: "", ITA: "", JPN: "",},
