30dd6217c80819b5a4eed24f9949bcb585659b78
Garesu <44981193+gperilli@users.noreply.github.com>
2020-04-22 00:19:56 +0900

    Update monojs-sitewide.js

--- js/monojs-sitewide.js
+++ js/monojs-sitewide.js
@@ -61,8 +61,6 @@ function setposter(pId) {
 			pIdX[i]["itaForv"] = pIdX[i]["WIcmp"]["ItaCmp1"].concat("_", pIdX[i]["WIcmp"]["ItaCmp2"], "_", pIdX[i]["WIcmp"]["ItaCmp3"], "/#it");
 		}
 		
-		//alert(pId["No"]);
-		
 	}
 	
 }
@@ -124,11 +122,6 @@ function AnnotBtnGen(P00XId1VarHead, P00XId5VarHead, P00XId6VarHead) {
 // Set Button Positions etc.
 function SetAnnotations(pId) {
 
-	//pId = window["p".concat(pno)];
-	//alert(pId.No);
-	//alert("Annotpno ".concat(pno));
-	//alert(pId.No);
-
     for(var i = 1; i < 9; ++i) {
 
 		var BtnContainerP00XBtnIDX = P00XId1VarHead.concat(i);
@@ -147,9 +140,6 @@ function SetAnnotations(pId) {
 	}
 }
 
-function SeAntPrt2() {
-	
-}
 
 //Header Template
 function sitewidetemps() {
@@ -353,6 +343,21 @@ ShareTw = `
    </div>
 </div>
 `;
+
+
+// Auto Audio speaker
+AutAudSpkr = "M52.546307 96.563052C50.574302 94.844602 45.153635 89.605852 40.500381 84.921382C40.500381 84.921382 32.039916 76.404164 32.039916 76.404164C32.039916 76.404164 22.733247 76.404164 22.733247 76.404164C10.032796 76.404164 11.125427 77.955252 11.125427 59.925843C11.125427 42.0607 10.031272 43.595831 22.764529 43.595831C22.764529 43.595831 31.954168 43.595831 31.954168 43.595831C31.954168 43.595831 42.838755 32.836325 42.838755 32.836325C48.825279 26.918594 54.379592 21.651537 55.181676 21.131753C56.42993 20.322827 56.869207 20.281627 58.231113 20.845747C59.193979 21.244579 59.97627 22.052941 60.212366 22.893028C60.71812 24.692615 60.72379 95.288012 60.218366 97.083232C59.876336 98.297992 57.957761 99.687493 56.622491 99.687493C56.352616 99.687493 54.518352 98.281492 52.546349 96.563052C52.546349 96.563052 52.546307 96.563052 52.546307 96.563052";
+
+// Auto Audio on waves
+AutAudOn1 = "M73.08867635 74.5106609363C71.8965220734 73.3185066596 71.6956953884 70.8791224404 72.691801355 69.6899394345C73.0556034338 69.2556206067 74.4780854367 68.008676414 75.8528710027 66.9189529278C78.7182396749 64.6477193522 79.744129787 62.0096806771 79.1438113779 58.4564218052C78.7791943825 56.2982790824 77.7649353536 55.0285092859 73.7245468212 51.671949599C71.7071994716 49.9960338076 71.4398750583 47.5844652338 73.08867635 45.9356612963C74.5657683523 44.4585719399 76.7021252837 44.6156127296 79.5302803105 46.4091754987C84.3041467504 49.4366702522 86.8237685103 54.2088777546 86.8237685103 60.2231611163C86.8237685103 66.2374471238 84.3041467504 71.0096519804 79.5302803105 74.0371493797C76.7021252837 75.8307121488 74.5657683523 75.9877529385 73.08867635 74.5106609363C73.08867635 74.5106609363 73.08867635 74.5106609363 73.08867635 74.5106609363";
+
+AutAudOn2 = "M79.5309179564 89.69009762C77.2187745063 87.2289329011 77.9946174966 85.7330581282 83.4448409487 82.1438053401C88.7394788195 78.6570177382 91.1574450182 75.9962222509 93.5070614053 71.0710776463C99.366150769 58.7895507385 94.8467155967 44.7067511034 82.8786511434 37.9523937719C78.3733711793 35.4097717831 77.2616634641 33.0484846253 79.4592926031 30.6896104675C80.9783819173 29.0590574881 83.211393785 29.2248427568 87.0247301328 31.2512864813C94.1326131266 35.0284886837 101.040420852 44.1682129019 102.968130015 52.346010028C103.987651607 56.6710611194 103.987651607 63.775263759 102.968130015 68.1003148504C101.026292102 76.3380586216 94.1918295217 85.377944966 87.0198088829 89.1950357513C83.168364598 91.2448474755 81.1188174572 91.3803353071 79.5309179564 89.69009762C79.5309179564 89.69009762 79.5309179564 89.69009762 79.5309179564 89.69009762";
+
+// Auto Audio off cross
+AutAudOff1 = "M 88.893387,62.509705 C 72.877577,46.493899 71.818667,45.318212 71.818667,43.551946 c 0,-2.446633 1.76836,-4.184194 4.25835,-4.184194 1.66892,0 3.13007,1.321183 18.8836,17.074713 15.468933,15.468922 17.074723,17.23786 17.074723,18.80947 0,1.136284 -0.4481,2.182849 -1.29887,3.033619 -0.85077,0.850771 -1.89733,1.298864 -3.03362,1.298864 -1.57161,0 -3.34054,-1.605791 -18.809463,-17.074713 z";
+
+AutAudOff2 = "m 73.071277,78.285554 c -1.47634,-1.476335 -1.63229,-2.880738 -0.54912,-4.945123 0.41237,-0.78591 8.25041,-8.751273 17.41789,-17.700805 16.967563,-16.564186 17.486373,-16.974211 20.173243,-15.943167 1.24389,0.477327 2.4779,3.165147 2.11571,4.608256 -0.17144,0.683059 -7.98357,8.900427 -17.360283,18.260817 -15.3988,15.371982 -17.2155,17.018886 -18.77358,17.018886 -1.12593,0 -2.17608,-0.45108 -3.02386,-1.298864 z";
+
 
 ColumnATail = `
 </div>
@@ -577,18 +582,18 @@ ITA: {lang: "ITA",
 	//Card Type
 	GlCrdTTxt: "tipo di carta: ", CrdTCLRTxt: "colore", CrdTBWTxt: "bianco e nero", GlMdeTTxt: "modalità : ",
 	//Header
-	HeadN: "[ITA]An SVG based visual dictionary, and language learning tool. Currently in English, Japanese, and Italian.", 
+	HeadN: "Un dizionario visuale basato su SVG e uno strumento di apprendimento delle lingue. Attualmente in inglese, giapponese e italiano.", 
 	//Footer
-	FootN1: "[ITA]Designed and coded by G.Perilli. Image data is either original or derived from public domain sources - in both cases the image files have been created or adapted by the author. Where possible all files have been attributed to their source.", 
-	FootN2: "[ITA]Example sentnces are where stated from the Tatoeba corpus licenced under Creative Commons CC-BY. Audio is provided by the ResponsiveVoice API.",
+	FootN1: "Progettato e codificato da G.Perilli. I dati di immagine sono originali o derivati ​​da fonti di dominio pubblico - in entrambi i casi i file di immagine sono stati creati o adattati dall'autore. Ove possibile, tutti i file sono stati attribuiti alla loro fonte.", 
+	FootN2: "Le frasi di esempio sono quelle indicate dal corpus Tatoeba concesso in licenza in base alla licenza Creative Commons CC-BY. L&#39;audio è fornito dall&#39;API ResponsiveVoice.",
 	//Site Copyright
-	CRText1: '&copy; G. Perilli', 
+	CRText1: "&copy; G. Perilli", 
 	 
 	//Text for image source attribution in modal
 	 
-	CRTextWI0: '[ITA]This is a derivative work. Attribution is provided for each individual component. &copy; G. Perilli', 
-	CRTextWIO: 'This is an original work. &copy; G. Perilli', 
-	CRTextWID: 'This is a derivative work. &copy; G. Perilli', 
+	CRTextWI0: "Questo è un opera derivata. L&#39;attribuzione è fornita per ogni singolo componente. &copy; G. Perilli", 
+	CRTextWIO: "Questa è un&#39;opera originale. &copy; G. Perilli", 
+	CRTextWID: "This is a derivative work. &copy; G. Perilli", 
 	},
 
 JPN: {lang: "JPN", 
@@ -602,47 +607,35 @@ JPN: {lang: "JPN",
 	HeadN: "SVGに基づいた図解辞典・語学学習ツールです。", 
 	//Footer
 	FootN1: "デザイン・コーディングはG.ペリーリによる。画像データはオリジナルかパブリック・ドメインのもので、どちらの場合も著者が作成または編集したものである。 すべてのファイルは可能なかざり資料元を記している。", 
-	FootN2: "例文はCC-BYのクリエイティブ・コモンズ・ライセンスを得た「たとえばコーパス」による。 Audio is provided by the ResponsiveVoice API.",
+	FootN2: "例文はCC-BYのクリエイティブ・コモンズ・ライセンスを得た「たとえばコーパス」による。 オーディオは、ResponsiveVoice APIによって提供されます。",
 	//Site Copyright
 	CRText1: '&copy; G. Perilli', 
 	
 	//Text for image source attribution in modal
 	
-	CRTextWI0: '[JPN]This is a derivative work. Attribution is provided for each individual component. &copy; G. Perilli', 
-	CRTextWIO: 'This is an original work. &copy; G. Perilli', 
-	CRTextWID: 'This is a derivative work. &copy; G. Perilli', 
+	CRTextWI0: 'これは派生作品です。属性は、個々のコンポーネントごとに提供されます。 &copy; G. Perilli', 
+	CRTextWIO: 'Tこれはオリジナル作品です。 &copy; G. Perilli', 
+	CRTextWID: 'これは派生作品です。 &copy; G. Perilli', 
 	},
 }
 
 }
 
 
-function MnuCntnt(L1, src) {
 
-	//Gallery index
-	//Share Section
-	if (src == "pst") {
-		//document.getElementById("menu-gallery").innerHTML = UItext[L1]["gallery"];
-		document.getElementById("menu-home").innerHTML = UItext[L1]["home"];
-	}
-	
+function uitext(L1) {
+	//uitext
+	document.getElementById("menu-home").innerHTML = UItext[L1]["home"];
+	document.getElementById("Brgrmenu-home").innerHTML = UItext[L1]["home"];
 	document.getElementById("menu-shareA").innerHTML = UItext[L1]["share"];
 	document.getElementById("menu-shareB").innerHTML = UItext[L1]["share"];
-	
 	document.getElementById("LSelectTxt").innerHTML = UItext[L1]["LSel"];
-	//document.getElementById("Brgrmenu-gallery").innerHTML = UItext[L1]["gallery"];
-	document.getElementById("Brgrmenu-home").innerHTML = UItext[L1]["home"];
-
-}
-
-function HdrFtrTxt(L1) {
-	
+	//header and footer
 	document.getElementById('Header').innerHTML = UItext[L1]["HeadN"];
 	document.getElementById('Footer1').innerHTML = UItext[L1]["FootN1"];
 	document.getElementById('Footer2').innerHTML = UItext[L1]["FootN2"];
-
 	document.getElementById('CRCont').innerHTML = UItext[L1]["CRText1"];
-	
+
 }
 
 
